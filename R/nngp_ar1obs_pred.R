65cdec5084e231c85a53772d3fac9e354a62fcec
nagahamaVH <victorh.nagahama@gmail.com>
2024-04-15 23:17:33 +0100

    prediction nngp ar1obs. improved function organisation and implemented posterior predictive dist

--- R/nngp_ar1obs_pred.R
+++ R/nngp_ar1obs_pred.R
@@ -1,39 +1,33 @@
-cov_partition <- function(coords, cov_function, sigma, l) {
-  n <- dim(coords)[1]
-  p <- dim(coords)[2]
-  D <- as.matrix(dist(coords[2:n,]))
-  d <- rowSums(sweep(coords[2:n,], 2, coords[1,])^p)^(1/p)
-
-  if (cov_function == "exp") {
-    cov12 <- sigma^2 * exp(-d / l)
-    cov22 <- sigma^2 * exp(-D / l)
+#' Construct covariance matrix
+#' @param coords1 A matrix. Each row is a site and number of columns corresponds
+#' to number of coordinates
+#' @param coords2 (Optional), vector/matrix. Reference coordinate to be computed
+#' covariance in relation to coords1
+cov_matrix <- function(coords1, sigma, l, cov_function, coords2 = NULL) {
+  if (is.null(coords2)) {
+    D <- as.matrix(dist(coords1))
+  } else {
+    p <- dim(coords1)[2]
+    coords2 <- if (is.vector(coords2)) matrix(coords2, ncol = p)
+    D <- rowSums(sweep(coords1, 2, coords2)^p)^(1 / p)
   }
   
-  return(list(cov12 = cov12, cov22 = cov22))
+  if (cov_function == "exp") {
+    cov <- sigma^2 * exp(-D / l)
+  }
+  
+  return(cov)
 }
 
-cond_mvn_par <- function(mu0, x, mu, sigma, l, tau, coords, cov_function) {
-  cov <- cov_partition(coords, cov_function, sigma, l)
-  cov11 <- sigma^2
+cond_mvn_par <- function(mu1, mu2, x2, cov11, cov12, cov22) {
+  invU22 <- chol2inv(chol(cov22))
+  cov12_invU22 <- tcrossprod(cov12, invU22)
   
-  invU22 <- chol2inv(chol(cov$cov22 + diag(tau^2, dim(coords)[1] - 1)))
-  cov12_invU22 <- tcrossprod(cov$cov12, invU22)
-
-  cond_mean <- mu0 + tcrossprod((x - mu), cov12_invU22)
-  cond_var <- cov11 - tcrossprod(cov$cov12, cov12_invU22)
+  cond_mean <- mu1 + tcrossprod(x2 - mu2, cov12_invU22)
+  cond_var <- cov11 - tcrossprod(cov12, cov12_invU22)
   
   return(list(mu = c(cond_mean), sigma2 = c(cond_var)))
 }
-
-# set.seed(124)
-# c <- coords[1:3, ]
-# d <- as.matrix(dist(c))
-# C <- sigma^2 * exp(-d / l)
-# mu1 <- rnorm(3)
-# x1 <- mu1 + crossprod(chol(C), rnorm(3))
-# condMVNorm::condMVN(mu1, C, dependent.ind = 1, given.ind = 2:3,
-# X.given = x1[2:3])
-# cond_mvn_par(mu1[1], x1[2:3], mu1[2:3], sigma, l, 0, c, "exp")
 
 #' Predict interpolation for given observed data of NN
 #'
@@ -54,53 +48,59 @@ cond_mvn_par <- function(mu0, x, mu, sigma, l, tau, coords, cov_function) {
 #' @param l Numeric. 
 #' @param tau Numeric. 
 #' @param cov_function Character.
-#' add coords <- cbind(coords_new, coords_obs) in general pred function
 pred_space <- function(X_pred, coords_pred, time_pred, y_obs, mu_obs, 
                        coords_obs, beta, rho, sigma, l, tau, cov_function) {
   T <- dim(y_obs)[1]
   n <- dim(y_obs)[2]
   pred_site_index <- which(coords_pred[1] == coords_obs[,1] & 
                              coords_pred[2] == coords_obs[,2])
-  
+
   if (length(pred_site_index) == 0) {
     max_t_krig <- min(T, time_pred) # Max time for kriging
-    coords <- rbind(coords_pred, coords_obs, deparse.level = 0)
     
-    pars1 <- cond_mvn_par(X_pred[1,] %*% beta, y_obs[1,], mu_obs[1,], sigma, l, 
-                          tau, coords, cov_function)
-    pred <- pars1$mu
+    cov11 <- sigma^2
+    cov22 <- (cov_matrix(coords_obs, sigma, l, cov_function) +
+      diag(tau^2, dim(coords_obs)[1]))
+    cov12 <- cov_matrix(coords_obs, sigma, l, cov_function, coords_pred)
     
+    Xbeta <- X_pred[1,] %*% beta
+    
+    pars1 <- cond_mvn_par(0, mu_obs[1,], y_obs[1,], cov11, cov12, cov22)
+    pred <- rnorm(1, Xbeta + pars1$mu, sqrt(pars1$sigma2))
+
     if (time_pred > 1) {
       y_pred_tm1 <- pred
       for (t in 2:max_t_krig) {
-        pars_t <- cond_mvn_par(
-          X_pred[t,] %*% beta + rho * (y_pred_tm1 - X_pred[t - 1,] %*% beta), 
-          y_obs[t,], mu_obs[t,], sigma, l, tau, coords, cov_function)
-        pred <- pars_t$mu
+        Xbeta <- X_pred[t,] %*% beta + 
+          rho * (y_pred_tm1 - X_pred[t - 1,] %*% beta)
+        pars_t <- cond_mvn_par(0, mu_obs[t,], y_obs[t,], cov11, cov12,
+                               cov22)
+        pred <- rnorm(1, Xbeta + pars_t$mu, sqrt(pars_t$sigma2))
         y_pred_tm1 <- pred
       }
       if (time_pred > T) {
+        cov22_w <- cov_matrix(coords_obs, sigma, l, cov_function)
+        U_w <- chol(cov22_w)
         for (t in (T + 1):time_pred) {
-          D <- as.matrix(dist(coords_obs))
-          L <- chol(sigma^2 * exp(-D / l))
-          w <- rnorm(n) %*% L
-          w_pars <- cond_mvn_par(0, w, rep(0, n), sigma, l, tau, coords, 
-                                 cov_function)
-          pred <- X_pred[t,] %*% beta + 
-            rho * (y_pred_tm1 - X_pred[t - 1,] %*% beta) + w_pars$mu
+          w <- rnorm(n) %*% U_w # Spatial field time t
+          w_pars <- cond_mvn_par(0, rep(0, n), w, sigma^2, cov12, cov22_w)
+          Xbeta <- X_pred[t,] %*% beta +
+            rho * (y_pred_tm1 - X_pred[t - 1,] %*% beta)
+          pred <- rnorm(1, Xbeta + w_pars$mu, tau)
           y_pred_tm1 <- pred
         }
       }
     }
   } else {
     y_pred_tm1 <- y_obs[T, pred_site_index]
+    cov22_w <- cov_matrix(coords_obs, sigma, l, cov_function)
+    U_w <- chol(cov22_w)
     for (t in (T + 1):time_pred) {
-      D <- as.matrix(dist(coords_obs))
-      L <- chol(sigma^2 * exp(-D / l))
-      w <- rnorm(n) %*% L
-      pred <- X_pred[t,] %*% beta + 
-        rho * (y_pred_tm1 - X_pred[t - 1,] %*% beta) + w[pred_site_index]
-      y_pred_tm1 <- pred 
+      w <- rnorm(n) %*% U_w # Spatial field time t
+      Xbeta <- X_pred[t,] %*% beta + 
+        rho * (y_pred_tm1 - X_pred[t - 1,] %*% beta)
+      pred <- rnorm(1, Xbeta + w[pred_site_index], tau)
+      y_pred_tm1 <- pred
     }
   }
   
